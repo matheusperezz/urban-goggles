232f550baaadcbe446008f00ff0d53052dd069ca
matheusperezz <matheus.mperez08@gmail.com>
2024-03-27 11:01:46 -0300

    finish problem 44

--- src/problems/dontmatter/44.py
+++ src/problems/dontmatter/44.py
@@ -12,24 +12,20 @@ def solve(records, max_distance):
         elif direction == 'O':
             x -= distance
         
-        # Calcula a distância atual da posição de Demasi à delegacia
         current_distance = sqrt(x**2 + y**2)
         
-        # Verifica se a distância atual excede a distância máxima permitida
         if current_distance > max_distance:
-            return 1  # Demasi se afastou além da distância permitida
+            return 1
     
-    return 0  # Demasi nunca se afastou além da distância permitida
+    return 0
 
 def main():
-    # Leitura da entrada
     n, max_distance = map(int, input().split())
     records = [input().split() for _ in range(n)]
+    records = [(direction, int(distance)) for direction, distance in records]
     
-    # Chamada da função para verificar se Demasi se afastou além da distância permitida
     result = solve(records, max_distance)
     
-    # Saída do resultado
     print(result)
 
 if __name__ == "__main__":
