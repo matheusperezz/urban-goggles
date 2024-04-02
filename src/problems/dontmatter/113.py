35c2b98013f7b054bf78d36a505970aca435bd99
matheusperezz <matheus.mperez08@gmail.com>
2024-04-02 19:34:07 -0300

    problem 113 finished

---
+++ src/problems/dontmatter/113.py
@@ -0,0 +1,14 @@
+ondas, clones_naruto = map(int, input().split())
+defendeu_vila = True
+for _ in range(ondas):
+    zetsus, clones = map(int, input().split())
+    clones_naruto += clones
+    if zetsus > clones_naruto:
+        defendeu_vila = False
+    clones_naruto -= zetsus   
+
+  
+if defendeu_vila:
+  print('Naruto defendeu a Vila')
+else:
+  print('Madara venceu')
