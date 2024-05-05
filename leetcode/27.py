9c60cb84885c1b2b0d8c3910213c2e59723e0c1f
matheusperezz <matheus.mperez08@gmail.com>
2024-05-05 20:24:41 -0300

    problems 27 and 26 and leetcode

--- leetcode/27.py
+++ leetcode/27.py
@@ -1,3 +1,13 @@
 class Solution:
-    def removeElement(self, nums: List[int], val: int) -> int:
-        
+    def removeElement(self, nums: list[int], val: int) -> int:
+        index = 0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[index] = nums[i]
+                index += 1
+        return index
+    
+nums = [3,2,2,3]
+val = 3
+r = Solution().removeElement(nums, val)
+print(f'Nums: {nums} Result: {r}')
